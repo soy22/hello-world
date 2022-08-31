fbb94fc0b8af60a7057ad3082c64d3cde27f5eb2
soy22 <111866530+soy22@users.noreply.github.com>
2022-08-31 18:39:26 +0900

    Delete 2-3.js

--- ch02/2-3.js
+++
@@ -1,9 +0,0 @@
-function a (x) {
-    var x=1;  // 수집 대상 1(매개변수 선언)
-    console.log(x);  // (1)
-    var x;  // 수집 대상 2(변수 선언)
-    console.log(x);  // (2)
-    var x=2;  // 수집 대상 3(변수 선언)
-    console.log(x);  // (3)
-}
-a();
