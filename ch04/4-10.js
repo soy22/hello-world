db64907454f3ae3a90995b91778f0e71bb7d0734
soy22 <jsy03070@naver.com>
2022-09-05 20:52:37 +0900

    예제 4-8의 func 함수 재활용

---
+++ ch04/4-10.js
@@ -0,0 +1,11 @@
+//...
+var obj2 = {
+    name: 'obj2',
+    func: obj1.func
+};
+var callback2 = obj2.func();
+setTimeout(callback2,1500);
+
+var obj3 = {name: 'obj3'};
+var callback3 = obj1.func.call(obj3);
+setTimeout(callback3,2000);
