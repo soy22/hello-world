7a53205ea9e3761a99d0198ac715892002586f88
soy22 <jsy03070@naver.com>
2022-09-07 23:58:11 +0900

    콜백 함수와 클로저(2)

---
+++ ch05/5-7.js
@@ -0,0 +1,12 @@
+//...
+var  alertFruit = function (fruit) {
+    alert('your choice is' + fruit);
+};
+fruits.forEach(function (fruit) {
+    var $li = document.createElement('li');
+    $li.innerText = fruit;
+    $li.addEventListener('click',alertFruit);
+    $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+alertFruit(fruits[1]);
